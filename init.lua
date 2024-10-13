df925f34165415a5a54dc3430507f5396a2d9e58
Vikas Chaurasia <whiskas101>
2024-10-13 19:42:31 +0530

    first commit

---
+++ init.lua
@@ -0,0 +1,35 @@
+
+
+
+--BASE CHANGES
+
+-- set relative line numbers 
+vim.opt.relativenumber = true
+
+-- syntax highlighting, because I am a mere mortal
+vim.cmd("syntax on")
+
+
+
+-- don't highlight last search option
+vim.opt.hlsearch = false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- more setup to be done to account for various packages
+--
+
+
+
+
